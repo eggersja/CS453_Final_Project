ea87c78da7699ca6d631d4571145a23075286da6
Eric Prather <pratherea@gmail.com>
2020-11-24 18:20:07 -0800

    Skeleton for traffic PLY generator.

--- transformer/boidsTransformer/BoidsExperiment.cs
+++ transformer/boidsTransformer/BoidsExperiment.cs
@@ -44,22 +44,61 @@ namespace boidsTransformer
         public List<BoidsSnapshot> Current { get; protected set; }
 
         /// <summary>
+        /// The number of boids in the experiment.
+        /// </summary>
+        public int BoidCount { 
+            get 
+            {
+                if (boidCount < 0)
+                    InitializeBoidCount();
+                return boidCount;
+            }
+        }
+
+        /// <summary>
+        /// The number of boids in the experiment. -1 if not yet calculated. Only access this through <see cref="BoidCount"/>.
+        /// </summary>
+        private int boidCount = -1;
+
+        public float[] Bounds
+        {
+            get
+            {
+                if (bounds == null)
+                    InitializeBounds();
+                return bounds;
+            }
+        }
+
+        /// <summary>
+        /// minX, minY, maxX, maxY
+        /// </summary>
+        private float[] bounds;
+
+        // TODO: Allow manual supply of bounds for visualization
+
+        /// <summary>
         /// Creates a BoidsExperiment from a boids log file.
         /// </summary>
         /// <param name="inputFile">The file the experiment can be found in.</param>
-        public BoidsExperiment(string inputFile)
+        /// <param name="experiment_borders">minX, minY, maxX, maxY. <c>null</c> to <see cref="InitializeBounds(float)">autocalculate</see> (lazy eval.)</param>
+        public BoidsExperiment(string inputFile, float[] experiment_borders = null)
         {
             string[] snapshotsRaw = File.ReadAllLines(inputFile); // Convenience function
-
-            InitializeSnapshot(snapshotsRaw);
-            
+            InitializeSnapshots(snapshotsRaw);
+            bounds = experiment_borders;
         }
 
         /// <summary>
         /// Initializes the boids experiment from a serialized representation.
         /// </summary>
         /// <param name="rawSnapshots">A serialized representation of the snapshot conforming to <see href="https://regex101.com/r/2eIGmk/6/"/>. May have multiple lines.</param>
-        public BoidsExperiment(string[] rawSnapshots) => InitializeSnapshot(rawSnapshots);
+        /// <param name="experiment_borders">minX, minY, maxX, maxY. <c>null</c> to <see cref="InitializeBounds(float)">autocalculate</see> (lazy eval.)</param>
+        public BoidsExperiment(string[] rawSnapshots, float[] experiment_borders = null)
+        {
+            InitializeSnapshots(rawSnapshots);
+            bounds = experiment_borders;
+        }
 
         /// <summary>
         /// Initializes the boids experiment from a serialized representation.
@@ -70,7 +109,7 @@ namespace boidsTransformer
         /// Span<byte> inputBuffer; // Alternative consideration if this were a more serious project.
         /// </remarks>
         /// <param name="snapshotsRaw">A serialized representation of the snapshot conforming to <see href="https://regex101.com/r/2eIGmk/6/"/>.  May have multiple lines.</param>
-        protected void InitializeSnapshot(string[] snapshotsRaw)
+        protected void InitializeSnapshots(string[] snapshotsRaw)
         {
             Current = new List<BoidsSnapshot>();
 
@@ -117,9 +156,66 @@ namespace boidsTransformer
             }
         }
 
-        public string ToPly()
+        /// <summary>
+        /// Sets <see cref="boidCount"/> based on the current state of the experiment.
+        /// </summary>
+        private void InitializeBoidCount()
         {
+            int maxBoids = 0;
+            foreach(BoidsSnapshot s in Current)
+            {
+                if (s.Coords.Count > maxBoids)
+                    maxBoids = s.Coords.Count;
+            }
+            boidCount = maxBoids;
+        }
+
+        private void InitializeBounds(float margin = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generates a grid PLY file based on traffic data sourced from this experiment. Intended for use in learnply by Dr. Eugene Zhang
+        /// </summary>
+        /// <param name="gridSize">The size of the .ply grid.</param>
+        /// <returns>Traffic ply file</returns>
+        public string TrafficPly(int gridSize = 20)
+        {
+            // Get the trafic density
+            float[][] gridWeights = ProjectTrafficGrid(gridSize);
+
+            // Format as .ply
+
+            // Done!
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// At a resolution determined by <paramref name="gridSize"/>, determine the relative traffic of all areas in the experiment.
+        /// </summary>
+        /// <param name="gridSize">The width and height of the grid the experiment should be superimposed upon.</param>
+        /// <param name="mode">The type of mapping to perform between vertices on the grid and the bots</param>
+        /// <returns>Traffic grid</returns>
+        protected static float[][] ProjectTrafficGrid(int gridSize = 20, MappingMode mode = MappingMode.SAME_QUAD)
+        {
+            float[][] gridWeights = new float[gridSize][];
+            for (int row = 0; row < gridSize; row++)
+            {
+                gridWeights[row] = new float[gridSize];
+                for (int col = 0; col < gridSize; col++)
+                {
+                    gridWeights[row][col] = 0;
+                }
+            }
+
+            return gridWeights;
+        }
+
+        public enum MappingMode
+        {
+            SAME_QUAD,
+            ALL_QUAD
         }
     }
 }
